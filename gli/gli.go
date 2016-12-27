695bc2ae8a5d322cabc5787e6915578878593665
Pieter Droogendijk <pieter@binky.org.uk>
2016-12-27 15:15:08 +0100

    Add gli.Viewport

--- gli/gli.go
+++ gli/gli.go
@@ -1,9 +1,16 @@
 package gli
 
 import (
+	"image"
 	"runtime"
+
+	"github.com/go-gl/gl/v2.1/gl"
 )
 
 func init() {
 	runtime.LockOSThread()
+}
+
+func Viewport(v image.Rectangle) {
+	gl.Viewport(int32(v.Min.X), int32(v.Min.Y), int32(v.Max.X), int32(v.Max.Y))
 }
